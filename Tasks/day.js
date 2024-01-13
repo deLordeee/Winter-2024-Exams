b774b47d088c6ba3a80a22d2b4bf240a91e622f6
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-13 16:30:42 +0200

    Added keyword const
    Added keyword const to array and function declarations

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 // Get day number
 
-let daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (s) => {
+const parseDay = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
