6a24fa99fd8b681451fdd918c4e38bdbbe4f900c
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 16:40:50 +0200

    Fixed naming
    Fixed naming of function and variables

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,14 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
+deleteDuplicates = (value, N) => {
   if (N <= 0) return [];
   else {
-    res = [];
+    result = [];
     for (let i = 0; i < N; i++) {
-      res[i] = value;
+      result[i] = value;
     }
-    return res;
+    return result;
   }
 };
 
-module.exports = duplicate;
+module.exports = deleteDuplicates;
