35fcf3f3932fda99d71a6f062c42cdcedf1cb01f
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 14:33:05 +0200

    Simplified for loop body
    Deleted unnecessary if statements

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -3,22 +3,10 @@
 const findLongestString = function (line = []) {
   let maxLength = 0;
   let longestString = 'Not found';
-  for (i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+  for (let string of line) {
+    if (string.length > maxLength) {
+      maxLength = string.length;
+      longestString = string;
     }
   }
   Object.assign(line, { length: s.length });
