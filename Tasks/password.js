db0765e898d0054c860a61a2c6587b94afad0acc
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 15:12:30 +0200

    Fixed spacing

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,10 +3,12 @@
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
   let password = '';
+  
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * max);
     password += alphabet[index];
   }
+  
   return password;
 };
 
