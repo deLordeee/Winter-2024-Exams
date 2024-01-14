aaf342909f9ecae8378dfb79e77d99aa80839c94
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 16:23:03 +0200

    Added keyword "const" and "let"

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,8 +1,8 @@
 // Sum all number values in dict
 
 const countNumberValues = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
     const value = obj[key];
