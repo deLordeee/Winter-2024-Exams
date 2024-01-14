186a151d88f33636b9b457467c5864cbad09562d
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 17:05:57 +0200

    Improved readability

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,10 +1,13 @@
 // Reverse an array, you can't use .reverse()
 'use strict';
- const reverseArray = (arr) => {
+
+const reverseArray = (arr) => {
   const resultArray = [];
+
   for (let i = arr.length - 1; i >= 0; i--) {
     resultArray.push(arr[i]);
   }
+
   return resultArray;
 };
 
