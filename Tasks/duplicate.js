4f8bdc23296268ec06ea2ee9554553151a794dfe
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 16:46:12 +0200

    Fixed spacing

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,13 +2,14 @@
 
 const deleteDuplicates = (value, N) => {
   if (N <= 0) return [];
+
+  const result = [];
+
+  for (let i = 0; i < N; i++) {
+    result[i] = value;
+  }
   
-    const result = [];
-    for (let i = 0; i < N; i++) {
-      result[i] = value;
-    }
-    return result;
-  
+  return result;
 };
 
 module.exports = deleteDuplicates;
