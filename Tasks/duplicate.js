226afd0fe9634fb4eefeef454c83a0445bb8dd7c
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 16:42:59 +0200

    Deleted unnecessary else statement

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,13 +2,13 @@
 
 const deleteDuplicates = (value, N) => {
   if (N <= 0) return [];
-  else {
+  
     const result = [];
     for (let i = 0; i < N; i++) {
       result[i] = value;
     }
     return result;
-  }
+  
 };
 
 module.exports = deleteDuplicates;
