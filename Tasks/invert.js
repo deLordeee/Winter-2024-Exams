3ad7abd62cb1c354478e906f7a09aa59a0e6747e
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 15:59:19 +0200

    Added keyword "const"

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,6 +1,6 @@
 // Reverse an array, you can't use .reverse()
 
-reverseArray = (A, i, j, k) => {
+ const reverseArray = (A, i, j, k) => {
   T = Object.keys(A, 4);
   T.forEach((_, i) => {
     T[i] = A.pop();
