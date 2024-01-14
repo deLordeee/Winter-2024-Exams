ee6cdc525f43b0ddff52e9566f5b523e2777207c
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 14:38:26 +0200

    Added keyword 'const' for function declaration

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,6 @@
 // Generate random integer value in given range
 
-getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
