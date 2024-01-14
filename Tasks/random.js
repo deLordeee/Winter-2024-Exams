ececaec22822e5566b7a30e9fb02381043052023
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 14:49:30 +0200

    Deleted unnecessary if\else statement

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,11 +1,10 @@
 // Generate random integer value in given range
 
 const getRandomInt = (min, max = min) => {
-  if (max === undefined) {
+  
     return Math.floor(Math.random() * (max + 1));
-  } else {
+  
     return min + Math.floor(Math.random() * (max - min + 1));
-  }
 };
 
 module.exports = getRandomInt;
