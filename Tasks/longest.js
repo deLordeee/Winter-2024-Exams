a36eff918e5174c555e09f9753649e0af0a8e326
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 17:08:35 +0200

    Fixed spacing

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,15 +1,18 @@
 // Find longest string
 'use strict';
+
 const findLongestString = function (line = []) {
   let maxLength = 0;
   let longestString = 'Not found';
+
   for (let string of line) {
     if (string.length > maxLength) {
       maxLength = string.length;
       longestString = string;
     }
   }
-  return longestString ;
+
+  return longestString;
 };
 
 module.exports = findLongestString;
