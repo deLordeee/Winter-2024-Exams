14a718398a948e8d3d2e070e35e7fb0b424eea16
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 15:48:22 +0200

    Changed variable and value

--- Tasks/range.js
+++ Tasks/range.js
@@ -3,7 +3,7 @@
 const generateArrayRange = (...range) => {
   let [from, to] = range;
   if (to >= from) {
-    range = new Array(to - from + 1);
+    const resultArray = [];
     for (let i = from; i <= to; i++) {
       range[i - from] = i;
     }
