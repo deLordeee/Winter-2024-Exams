2755f5c743f8964e194f5e6452243e1cb9fa321f
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 14:44:36 +0200

    Set default parameter value
    Simplified code by setting default parameter value max =min

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,8 +1,7 @@
 // Generate random integer value in given range
 
-const getRandomInt = (min, max) => {
+const getRandomInt = (min, max = min) => {
   if (max === undefined) {
-    max = min;
     return Math.floor(Math.random() * (max + 1));
   } else {
     return min + Math.floor(Math.random() * (max - min + 1));
