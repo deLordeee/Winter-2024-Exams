01e82a4610212ef618f2f5de2b10616bc810ed3c
Yevhenii Butkevych(IM-34) <143118683+deLordeee@users.noreply.github.com>
2024-01-14 15:08:16 +0200

    Added keywords "const" and "let"

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,10 @@
 // Generate random password
 
-let generatePassword = (alphabet, length) => {
+const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
-  password = '';
+  let password = '';
   for (let i = 0; i < length; i++) {
-    index = Math.floor(Math.random() * max);
+    const index = Math.floor(Math.random() * max);
     password = password + alphabet[index];
   }
   return password;
